f6658938756e41c15733adc37250694c35168b80
SaltWood_233 <105980161+SALTWOOD@users.noreply.github.com>
2026-08-08 22:05:49 +0800

    chore(site): update site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '盐木的小破窝';
+export const SITE_TITLE = '盐林格勒';
 export const SITE_ALTERNATE_NAME = '盐木';
-export const SITE_DESCRIPTION = '盐木的个人博客，记录游戏、运维、安全与逆向工程的实践与思考';
+export const SITE_DESCRIPTION = '梦想是成为一位厉害的曲师！ヽ(≧∀≦)ﾉ';
 export const SITE_AUTHOR = 'SALTWOOD';
 export const SITE_AUTHOR_ALTERNATE_NAME = '盐木';
 export const SITE_BIO = '咕咕咕……';
